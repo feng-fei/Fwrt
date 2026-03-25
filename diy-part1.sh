6b3f6f645cb03308b05707f01891a8fd842f8b75
feng-fei <61817415+feng-fei@users.noreply.github.com>
2026-03-25 10:38:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # 添加额外插件源
 # sed -i '$a src-git custom https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# 添加 Lucky 插件源
+git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
