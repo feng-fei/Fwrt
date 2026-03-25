98d9be93e7ba4deaee9a2dacfb990e2472872f6e
feng-fei <61817415+feng-fei@users.noreply.github.com>
2026-03-25 12:22:10 +0800

    Add luci-app-aurora and related packages

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,3 +3,6 @@
 # sed -i '$a src-git custom https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # 添加 Lucky 插件源
 git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
+#luci-app-aurora 增加
+git clone https://github.com/eamonxg/luci-theme-aurora.git package/luci-theme-aurora
+git clone https://github.com/eamonxg/luci-app-aurora-config.git package/luci-app-aurora-config
